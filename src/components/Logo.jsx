61de535fea3ec36e1c293f5672908d09fb8147e5
Jatin Kundara <152637042+JatinKundara@users.noreply.github.com>
2024-11-08 11:30:14 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo(props) {
   return (
-    <img className='w-[130px] lg:w-[230px]' {...props} src="src/images/logo.png" alt="" />
+    <img className='w-[130px] lg:w-[230px]' {...props} src="../images/logo.png" alt="" />
   )
 }
 
